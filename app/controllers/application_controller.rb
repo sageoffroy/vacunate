de87ffc7160bc0cb814c86ebcc307edbc1471667
Silvio Geoffroy <sageoffroy@gmail.com>
2021-01-30 21:31:56 -0300

    Prueba

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,4 @@ class ApplicationController < ActionController::Base
 	def authenticate_user!
     	redirect_to root_path, notice: "Debes tener permisos para poder ingresar a esa página" unless user_signed_in?
   	end
-
-
-  	
-
 end
