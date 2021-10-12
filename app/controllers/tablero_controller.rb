b8c08e2020a3ccdabac9f999bcbc6e42ef6e3eaf
Silvio Geoffroy <sageoffroy@gmail.com>
2021-10-12 09:59:06 -0300

    Tablero muestra de personal

--- app/controllers/tablero_controller.rb
+++ app/controllers/tablero_controller.rb
@@ -35,10 +35,10 @@ class TableroController < ApplicationController
   	@total_18_59_con_riesgo = inscripciones.where(population_group: "Tengo entre 18 y 59 (con factores de riesgo)").count
     @total_18_59_sin_riesgo = inscripciones.where(population_group: "Tengo entre 18 y 59 (sin factores de riesgo)").count
     @total_18_59 = @total_18_59_con_riesgo + @total_18_59_sin_riesgo
-    total_educacion = inscripciones.where(population_group: "Soy personal docente/auxiliar").count
-    total_seguridad = inscripciones.where(population_group: "Soy personal de seguridad").count
-    total_salud = inscripciones.where(population_group: "Soy personal de salud").count
-    @total_administracion = total_educacion + total_seguridad + total_salud
+    @total_educacion = inscripciones.where(population_group: "Soy personal docente/auxiliar").count
+    @total_seguridad = inscripciones.where(population_group: "Soy personal de seguridad").count
+    @total_salud = inscripciones.where(population_group: "Soy personal de salud").count
+    @total_administracion = @total_educacion + @total_seguridad + @total_salud
 
 
     a = inscripciones.group(:locality).order('count_id desc').count('id')
